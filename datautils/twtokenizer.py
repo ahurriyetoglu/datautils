53933b32e1c92e80ea31723b8cf40adabae0683c
ahurriyetoglu <ali.hurriyetoglu@gmail.com>
2015-04-20 00:51:02 +0200

    Update twtokenizer.py

--- datautils/twtokenizer.py
+++ datautils/twtokenizer.py
@@ -24,7 +24,7 @@ class twtokenizer():
                 newtw += ' '+c+' '
             elif (c in "'`´’()+*-") and ((lentw>i+1) and (i>0)) and (tw[i-1] in ' 0123456789') and (tw[i+1].isalpha()):
                 newtw += c+' '
-            elif (c in '();>:') and ((lentw>i+1) and (i!=0)) and ((tw[i-1].isalpha()) or (tw[i-1] in '0123456789')) and ((tw[i+1] == ' ') or (i == lentw-1)):
+            elif (c in '();>:.') and ((lentw>i+1) and (i!=0)) and ((tw[i-1].isalpha()) or (tw[i-1] in '0123456789')) and ((tw[i+1] == ' ') or (i == lentw-1)):
                 newtw += ' '+c
             elif (c in "'`´’‘()+*->") and (i==0) and (lentw > 1) and ((tw[1].isalpha()) or tw[1] in "0123456789"):
                 newtw += c+' '
