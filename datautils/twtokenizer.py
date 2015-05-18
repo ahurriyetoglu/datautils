f4c3522410c5e130b50e1bfe17fe55c963d185c7
Ali Hürriyetoğlu <ali.hurriyetoglu@gmail.com>
2015-05-18 16:02:29 +0200

    Update twtokenizer.py

--- datautils/twtokenizer.py
+++ datautils/twtokenizer.py
@@ -62,7 +62,7 @@ class twtokenizer():
         tokdf[newtexcol][tokdf[newtexcol].str.endswith(".'")] = tokdf[tokdf[newtexcol].str.endswith(".'")][newtexcol].apply(lambda tw: tw[:-2]+" . '") 
         tokdf[newtexcol][tokdf[newtexcol].str.startswith("'")] = tokdf[tokdf[newtexcol].str.startswith("'")][newtexcol].apply(lambda tw: "' "+tw[1:])
     
-        tokdf[newtexcol] = tokdf[newtexcol].apply(tokenize)
+        tokdf[newtexcol] = tokdf[newtexcol].apply(self.tokenize)
         tokdf[newtexcol] = tokdf[newtexcol].str.strip()
         tokdf[rescol] = tokdf[newtexcol].str.split()
         
